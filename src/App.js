c561033940af2c049a340b3a902d72698556ebc1
Nyam2C <qkrxogus113@naver.com>
2025-10-20 09:35:53 +0900

    feat: 문자열 파싱 기능

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Console } from "@woowacourse/mission-utils";
+import Calculator from "./Calculator.js";
 
 class App {
   async run() {
     const input = await Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+
+    const calculator = new Calculator();
+    const result = calculator.calculate(input);
+
+    Console.print(`결과 : ${result}`);
   }
 }
 
