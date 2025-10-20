fcd36e05936e3a9f654d66a6798b231abe9e9c65
Nyam2C <qkrxogus113@naver.com>
2025-10-20 17:47:14 +0900

    feat: 커스텀 구분자 지정

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,13 +1,25 @@
 class Calculator {
   calculate(input) {
-
-    const numbers = this.splitByDelimiter(input);
+    const { delimiter, numbersString } = this.parseInput(input);
+    const numbers = this.splitByDelimiter(numbersString, delimiter);
 
     return this.sum(numbers);
   }
 
-  splitByDelimiter(numbersString) {
-    const regex = new RegExp("[,:]");
+  parseInput(input) {
+    if (input.startsWith("//")) {
+      let delimiterEndIndex = input.indexOf("\\n");
+
+      const delimiter = input.substring(2, delimiterEndIndex);
+      const numbersString = input.substring(delimiterEndIndex + 2);
+      return { delimiter, numbersString };
+    }
+
+    return { delimiter: ",:", numbersString: input };
+  }
+
+  splitByDelimiter(numbersString, delimiter) {
+    const regex = new RegExp(`[${delimiter}]`);
     return numbersString.split(regex).filter((str) => str !== "");
   }
 
